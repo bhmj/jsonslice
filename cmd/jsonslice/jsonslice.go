26c0226b546c9fdf3d72f580777212ff2aacec55
BHMJ <m.tompson@gmail.com>
2019-03-01 21:33:35 +0300

    cmd tools update

---
+++ cmd/jsonslice/jsonslice.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+
+	"github.com/bhmj/jsonslice"
+)
+
+func main() {
+
+	if len(os.Args) < 2 {
+		fmt.Printf("Usage: %[1]s jsonpath [input_file]\n  ex.1: %[1]s '$.store.book[0].author' sample0.json\n  ex.2: cat sample0.json | %[1]s '$.store.book[0].author'\n", filepath.Base(os.Args[0]))
+		return
+	}
+
+	var data []byte
+	var err error
+
+	if len(os.Args) == 2 {
+		data, err = ioutil.ReadAll(os.Stdin)
+	} else {
+		data, err = ioutil.ReadFile(os.Args[2])
+	}
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	s, err := jsonslice.Get(data, os.Args[1])
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println(string(s))
+}
